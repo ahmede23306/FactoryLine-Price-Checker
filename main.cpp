55ffb0d89ab45f28c076a9361b7ab9adad94a8e1
Ahmed Eid <ahmede23306@gmail.com>
2025-03-18 15:09:07 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,142 +1,154 @@
-#include<iostream>  
-using namespace std;  
-int prod, prices;
+#include <iostream>
+using namespace std;
 
-struct product  
-{  
-   double baseprice, taxes, maxprice, netprice, totalsales;  
-   int productserial, quantity;  
-};  
+int qty, prices;
+const int prod = 4;
 
-void price(product* products, int &i)  
-{  
-   cout << "Enter the base price of the product: ";  
-   cin >> products[i].baseprice;  
-   cout << "Enter the taxes of the product: ";  
-   cin >> products[i].taxes;  
-   cout << "Enter the maximum price of the product: ";  
-   cin >> products[i].maxprice;  
-   products[i].netprice = products[i].baseprice + products[i].baseprice * (products[i].taxes / 100);  
-   cout << "The net price of the product is: " << products[i].netprice << endl;  
-   if (products[i].netprice > products[i].maxprice)  
-   {  
-       cout << "The product net price is accepted." << endl;  
-   }  
-   else {  
-       cout << "The product net price is not accepted." << endl;  
-   }  
-}  
+struct product {
+    int productserial, quantity;
+    double price;
+    int dailysales[30] = { 0 };
+};
 
-void products_data(product* products, int &i)  
-{  
-   cout << "Data of product number #" << i + 1 << endl;  
-   cout << "Enter the product serial number: ";  
-   cin >> products[i].productserial;  
-   cout << "Enter the quantity of the product: ";  
-   cin >> products[i].quantity;  
-   cout << "Enter the total sales of the product: ";  
-   cin >> products[i].totalsales;  
-}  
+product* products = new product[prod];
 
-void lessthanqty(product* products, int &qty)  
-{  
-   for (int i = 0; i < prod; i++)  
-   {  
-       if (products[i].quantity < qty)  
-       {  
-           cout << "Product: " << products[i].productserial << endl;  
-       }  
-   }  
-}  
-
-void lessthanprice(product* products, int &price)
-{
-	for (int i = 0; i < prod; i++)
-	{
-		if (products[i].netprice < prices)
-		{
-			cout << "Product: " << products[i].productserial << endl;
-		}
-	}
+void products_data(product* products) {
+    for (int x = 0; x < prod; x++) {
+        cout << "Data of product number #" << x + 1 << endl;
+        cout << "Enter the product serial number: ";
+        cin >> products[x].productserial;
+        cout << "Enter the quantity of the product: ";
+        cin >> products[x].quantity;
+        cout << "Enter the price of the product: ";
+        cin >> products[x].price;
+        cout << "Enter the daily sales of the last month of the product: ";
+        for (int n = 0; n < 30; n++) {
+            cout << "Day #" << n + 1 << " sales: ";
+            cin >> products[x].dailysales[n];
+        }
+    }
 }
 
-int highersalesproduct(product* products)  
-{  
-   int maxIndex = 0;  
-   for (int i = 1; i < prod; i++)  
-   {  
-       if (products[i].totalsales > products[maxIndex].totalsales)  
-       {  
-           maxIndex = i;  
-       }  
-   }  
-   return products[maxIndex].productserial;  
-}  
+void lessthanqty(product* products, int& qty) {
+    for (int i = 0; i < prod; i++) {
+        if (products[i].quantity < qty) {
+            cout << "Product: " << products[i].productserial << endl;
+        }
+    }
+}
 
-int main()  
-{  
-   cout << "Enter the number of products: ";  
-   cin >> prod;  
-   product* products = new product[prod];  
+void lessthanprice(product* products, int& price) {
+    for (int i = 0; i < prod; i++) {
+        if (products[i].price < prices) {
+            cout << "Product: " << products[i].productserial << endl;
+        }
+    }
+}
 
-   for (int i = 0; i < prod; i++)  
-   {  
-       products_data(products, i);  
-       price(products, i);  
-   }  
-   char continueChoice;  
-   int highestSalesSerial; // Declare outside the switch-case  
-   do {  
-       cout << "Please enter a number:\nPress 1 to get products that have less quantity than a certain value\nPress 2 to Get Product with the highest sales\nPress 3 to Apply 50 % discount for products that have quantity less than 6\nPress 4 to Display all the products\nPress 5 to know products under specific price" << endl;  
-       int choice;  
-       cin >> choice;  
-       int qty; // Declare qty outside the switch-case  
-       switch (choice)  
-       {  
-       case 1:  
-           cout << "Serial Numbers of the Products that has less quantity than a value entered by the user: " << endl;  
-           cout << "Enter the value of the quantity: ";  
-           cin >> qty;  
-           lessthanqty(products, qty);  
-           break;  
-       case 2:  
-           highestSalesSerial = highersalesproduct(products);  
-           cout << "The product with the highest sales has the serial number: " << highestSalesSerial << endl;  
-           break;  
-       case 3:  
-           for (int i = 0; i < prod; i++)  
-           {  
-               if (products[i].quantity < 6)  
-               {  
-                   products[i].netprice = products[i].netprice - products[i].netprice * 0.5;  
-                   cout << "The new price of the product " << i + 1 << " with serial number " << products[i].productserial << " is: " << products[i].netprice << endl;  
-               }  
-           }  
-           break;  
-       case 4:  
-           for (int i = 0; i < prod; i++)  
-           {  
-               cout << "Product number #" << i + 1 << endl;  
-               cout << "Product serial number: " << products[i].productserial << endl;  
-               cout << "Product quantity: " << products[i].quantity << endl;  
-               cout << "Product total sales: " << products[i].totalsales << endl;  
-               cout << "Product base price: " << products[i].baseprice << endl;  
-               cout << "Product taxes: " << products[i].taxes << endl;  
-               cout << "Product maximum price: " << products[i].maxprice << endl;  
-               cout << "Product net price: " << products[i].netprice << endl;  
-           }
-       case 5:
-           cout << "Serial Numbers of the Products that has less price than a value entered by the user: " << endl;
-		   cout << "Enter the value of the price: ";
-		   cin >> prices;
-		   lessthanprice(products, prices);
-           break;  
-       default:  
-           cout << "Invalid choice" << endl;  
-           break;
-       }
-       cout << "Do you want to enter the data of another product? (y/n): ";  
-       cin >> continueChoice;
-   } while (continueChoice == 'y' || continueChoice == 'Y');
-   delete[] products;
+void highersales(product* products, int& highestSalesSerial) {
+    int highestSales = 0;
+    for (int i = 0; i < prod; i++) {
+        int totalSales = 0;
+        for (int j = 0; j < 30; j++) {
+            totalSales += products[i].dailysales[j];
+        }
+        if (totalSales > highestSales) {
+            highestSales = totalSales;
+            highestSalesSerial = products[i].productserial;
+        }
+    }
+    cout << "The product with the highest sales is: " << highestSalesSerial << endl;
+}
+
+void totalsalesofeachproduct(product* products, int& i) {
+    int totalSales = 0;
+    for (int j = 0; j < 30; j++) {
+        totalSales += products[i].dailysales[j];
+    }
+    cout << "The total sales of product " << i + 1 << " is: " << totalSales << endl;
+}
+
+void dailysalespreview(product* products, int& i) {
+    for (int j = 0; j < 30; j++) {
+        cout << "Day #" << j + 1 << ": " << products[i].dailysales[j] << endl;
+    }
+}
+
+void avaragemonthlysales(product* products) {
+    for (int i = 0; i < prod; i++) {
+        int totalSales = 0;
+        for (int j = 0; j < 30; j++) {
+            totalSales += products[i].dailysales[j];
+        }
+        cout << "The average monthly sales for product " << i + 1 << " is: " << static_cast<double>(totalSales) / 30 << endl;
+    }
+}
+
+void selectingoperation(product* products, int& qty) {
+    char continueChoice;
+    int highestSalesSerial;
+    do {
+        cout << "Please enter a number:" << endl;
+        cout << "Press 1 to get products that have less quantity than a certain value" << endl;
+        cout << "Press 2 to Get Product with the highest sales" << endl;
+        cout << "Press 3 to Apply 50 % discount for products that have quantity less than 6" << endl;
+        cout << "Press 4 to Display all the products" << endl;
+        cout << "Press 5 to know products under specific price" << endl;
+        cout << "Press 6 to get the average monthly sales for the products" << endl;
+        cout << "Enter your choice: ";
+        int choice;
+        cin >> choice;
+        switch (choice) {
+        case 1:
+            cout << "Serial Numbers of the Products that has less quantity than a value entered by the user: " << endl;
+            cout << "Enter the value of the quantity: ";
+            cin >> qty;
+            lessthanqty(products, qty);
+            break;
+        case 2:
+            highersales(products, highestSalesSerial);
+            break;
+        case 3:
+            for (int i = 0; i < prod; i++) {
+                if (products[i].quantity < 6) {
+                    products[i].price = products[i].price - products[i].price * 0.5;
+                    cout << "The new price of the product " << i + 1 << " with serial number " << products[i].productserial << " is: " << products[i].price << endl;
+                }
+            }
+            break;
+        case 4:
+            for (int i = 0; i < prod; i++) {
+                cout << "Product number #" << i + 1 << endl;
+                cout << "Product serial number: " << products[i].productserial << endl;
+                cout << "Product quantity: " << products[i].quantity << endl;
+                cout << "Product daily sales: " << endl;
+                dailysalespreview(products, i);
+                cout << "The total sales: " << endl;
+                totalsalesofeachproduct(products, i);
+                cout << "Product price: " << products[i].price << endl;
+            }
+            break;
+        case 5:
+            cout << "Serial Numbers of the Products that has less price than a value entered by the user: " << endl;
+            cout << "Enter the value of the price: ";
+            cin >> prices;
+            lessthanprice(products, prices);
+            break;
+        case 6:
+            avaragemonthlysales(products);
+            break;
+        default:
+            cout << "Invalid choice" << endl;
+            break;
+        }
+        cout << "Do you want to enter the data of another product? (y/n): ";
+        cin >> continueChoice;
+    } while (continueChoice == 'y' || continueChoice == 'Y');
+}
+
+int main() {
+    products_data(products);
+    selectingoperation(products, qty);
+    delete[] products;
+    return 0;
 }
